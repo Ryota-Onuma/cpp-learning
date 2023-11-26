d5d90bffe4bec4afd9e570b46426d53ba98f7980
Ryota-Onuma <onumaryotajh.tech@gmail.com>
2023-11-26 19:54:28 +0900

    feat: ✨ std::array使ってみる

--- src/array.cpp
+++ src/array.cpp
@@ -1,23 +1,28 @@
 #include <iostream>
 #include "array.hpp"
+#include <array>
 using namespace std;
 
 void playWithArray(){
-  int array[5] = {10, 20, 30, 40, 50};
+  int normalArray[5] = {10, 20, 30, 40, 50};
   for (int i = 0; i < 5; i++) {
-    cout << "配列:" << array[i] << "\n";
+    cout << "配列:" << normalArray[i] << "\n";
   }
 
-  int* p = array;
+  int* p = normalArray;
 
   cout << "配列のポインタ: " << *p << "\n";
   cout << "配列のポインタ2: " << *(p + 1) << "\n";
   cout << "p" << p << "\n";
-  cout << "配列のインデックス指定: " << array[0] << "\n";
+  cout << "配列のインデックス指定: " << normalArray[0] << "\n";
 
-  testArrayPass(array, 5);
-  array[4] = 100;
-  testArrayPass(array, 5);
+  testArrayPass(normalArray, 5);
+  normalArray[4] = 100;
+  testArrayPass(normalArray, 5);
+  auto stdArray = testArrayPass2();
+  for (int i = 0; i < stdArray.size(); i++) {
+    cout << stdArray[i] << "\n";
+  }
 }
 
 void testArrayPass(const int array[5], size_t size){
@@ -26,4 +31,16 @@ void testArrayPass(const int array[5], size_t size){
   }
   cout << "サイズ: " << size << "\n";
   cout << "配列[" << size << "]: " << array[size-1] << "\n";
+}
+
+array<int,5> testArrayPass2() {
+  array<int, 5> stdArray = {10, 20, 30, 40, 50};
+  auto size = stdArray.size();
+  cout << "サイズ: " << size << "\n";
+  cout <<  "stdArray[0]: " << stdArray[0] << "\n";
+  stdArray[0] = 100;
+  cout <<  "更新後 stdArray[0]: " << stdArray[0] << "\n";
+  array<int,5> stdArray2 = stdArray;
+  cout << "stdArray2[0]: " << stdArray2[0] << "\n";
+  return stdArray2;
 }
--- src/array.hpp
+++ src/array.hpp
@@ -1,9 +1,11 @@
 #ifndef CONTAINER_H_
 #define CONTAINER_H_
 #include <iostream>
+#include <array>
 using namespace std;
 
 void playWithArray();
 void testArrayPass(const int array[5], size_t size);
+array<int,5> testArrayPass2();
 
 #endif  // CONTAINER_H_
